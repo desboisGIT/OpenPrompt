dc06d75b8822760e78ff170795a73b33806264c1
Ethan <ethan.pelletier@gradison.ca>
2025-01-03 16:02:13 -0800

    feat: landing impl.

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,132 @@
+import LandingCard from "@/components/custom/card.landing";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const BLOG_WRITER_ICON = (
+  <svg
+    width="15"
+    height="15"
+    className="size-10"
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.2 1H4.17741H4.1774C3.86936 0.999988 3.60368 0.999978 3.38609 1.02067C3.15576 1.04257 2.92825 1.09113 2.71625 1.22104C2.51442 1.34472 2.34473 1.51442 2.22104 1.71625C2.09113 1.92825 2.04257 2.15576 2.02067 2.38609C1.99998 2.60367 1.99999 2.86935 2 3.17738V3.1774V3.2V11.8V11.8226V11.8226C1.99999 12.1307 1.99998 12.3963 2.02067 12.6139C2.04257 12.8442 2.09113 13.0717 2.22104 13.2837C2.34473 13.4856 2.51442 13.6553 2.71625 13.779C2.92825 13.9089 3.15576 13.9574 3.38609 13.9793C3.60368 14 3.86937 14 4.17741 14H4.2H10.8H10.8226C11.1306 14 11.3963 14 11.6139 13.9793C11.8442 13.9574 12.0717 13.9089 12.2837 13.779C12.4856 13.6553 12.6553 13.4856 12.779 13.2837C12.9089 13.0717 12.9574 12.8442 12.9793 12.6139C13 12.3963 13 12.1306 13 11.8226V11.8V3.2V3.17741C13 2.86936 13 2.60368 12.9793 2.38609C12.9574 2.15576 12.9089 1.92825 12.779 1.71625C12.6553 1.51442 12.4856 1.34472 12.2837 1.22104C12.0717 1.09113 11.8442 1.04257 11.6139 1.02067C11.3963 0.999978 11.1306 0.999988 10.8226 1H10.8H4.2ZM3.23875 2.07368C3.26722 2.05623 3.32362 2.03112 3.48075 2.01618C3.64532 2.00053 3.86298 2 4.2 2H10.8C11.137 2 11.3547 2.00053 11.5193 2.01618C11.6764 2.03112 11.7328 2.05623 11.7613 2.07368C11.8285 2.11491 11.8851 2.17147 11.9263 2.23875C11.9438 2.26722 11.9689 2.32362 11.9838 2.48075C11.9995 2.64532 12 2.86298 12 3.2V11.8C12 12.137 11.9995 12.3547 11.9838 12.5193C11.9689 12.6764 11.9438 12.7328 11.9263 12.7613C11.8851 12.8285 11.8285 12.8851 11.7613 12.9263C11.7328 12.9438 11.6764 12.9689 11.5193 12.9838C11.3547 12.9995 11.137 13 10.8 13H4.2C3.86298 13 3.64532 12.9995 3.48075 12.9838C3.32362 12.9689 3.26722 12.9438 3.23875 12.9263C3.17147 12.8851 3.11491 12.8285 3.07368 12.7613C3.05624 12.7328 3.03112 12.6764 3.01618 12.5193C3.00053 12.3547 3 12.137 3 11.8V3.2C3 2.86298 3.00053 2.64532 3.01618 2.48075C3.03112 2.32362 3.05624 2.26722 3.07368 2.23875C3.11491 2.17147 3.17147 2.11491 3.23875 2.07368ZM5 10C4.72386 10 4.5 10.2239 4.5 10.5C4.5 10.7761 4.72386 11 5 11H8C8.27614 11 8.5 10.7761 8.5 10.5C8.5 10.2239 8.27614 10 8 10H5ZM4.5 7.5C4.5 7.22386 4.72386 7 5 7H10C10.2761 7 10.5 7.22386 10.5 7.5C10.5 7.77614 10.2761 8 10 8H5C4.72386 8 4.5 7.77614 4.5 7.5ZM5 4C4.72386 4 4.5 4.22386 4.5 4.5C4.5 4.77614 4.72386 5 5 5H10C10.2761 5 10.5 4.77614 10.5 4.5C10.5 4.22386 10.2761 4 10 4H5Z"
+      fill="currentColor"
+      fillRule="evenodd"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+const GAME_SUGGESTION_ICON = (
+  <svg
+    className="size-10"
+    width="15"
+    height="15"
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13.5 4H1.5C1.22386 4 1 4.22386 1 4.5V10.5C1 10.7761 1.22386 11 1.5 11H13.5C13.7761 11 14 10.7761 14 10.5V4.5C14 4.22386 13.7761 4 13.5 4ZM1.5 3C0.671573 3 0 3.67157 0 4.5V10.5C0 11.3284 0.671573 12 1.5 12H13.5C14.3284 12 15 11.3284 15 10.5V4.5C15 3.67157 14.3284 3 13.5 3H1.5ZM2 5H3V6H2V5ZM5 5H4V6H5V5ZM6 5H7V6H6V5ZM9 5H8V6H9V5ZM10 5H11V6H10V5ZM13 5H12V6H13V5ZM11 7H12V8H11V7ZM13 9H12V10H13V9ZM9 7H10V8H9V7ZM8 7H7V8H8V7ZM5 7H6V8H5V7ZM4 7H3V8H4V7ZM2 9H3V10H2V9ZM11 9H4V10H11V9Z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+const DEVELOPER_ICON = (
+  <svg
+    className="size-10"
+    width="15"
+    height="15"
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9.96424 2.68571C10.0668 2.42931 9.94209 2.13833 9.6857 2.03577C9.4293 1.93322 9.13832 2.05792 9.03576 2.31432L5.03576 12.3143C4.9332 12.5707 5.05791 12.8617 5.3143 12.9642C5.5707 13.0668 5.86168 12.9421 5.96424 12.6857L9.96424 2.68571ZM3.85355 5.14646C4.04882 5.34172 4.04882 5.6583 3.85355 5.85356L2.20711 7.50001L3.85355 9.14646C4.04882 9.34172 4.04882 9.6583 3.85355 9.85356C3.65829 10.0488 3.34171 10.0488 3.14645 9.85356L1.14645 7.85356C0.951184 7.6583 0.951184 7.34172 1.14645 7.14646L3.14645 5.14646C3.34171 4.9512 3.65829 4.9512 3.85355 5.14646ZM11.1464 5.14646C11.3417 4.9512 11.6583 4.9512 11.8536 5.14646L13.8536 7.14646C14.0488 7.34172 14.0488 7.6583 13.8536 7.85356L11.8536 9.85356C11.6583 10.0488 11.3417 10.0488 11.1464 9.85356C10.9512 9.6583 10.9512 9.34172 11.1464 9.14646L12.7929 7.50001L11.1464 5.85356C10.9512 5.6583 10.9512 5.34172 11.1464 5.14646Z"
+      fill="currentColor"
+      fillRule="evenodd"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
 export default function Home() {
-  return <>Hello World</>;
+  return (
+    <div className="bg-white text-black flex flex-col h-screen py-2 justify-between">
+      <div className="absolute inset-x-0 top-0 flex flex-col z-50 h-screen justify-between">
+        {/* Navigation */}
+        <div className="flex justify-between py-6 px-24 items-center">
+          <span className="font-bold text-xl">OpenPrompt</span>
+          <div className="flex space-x-4">
+            <span>Trending</span>
+            <span>Staff Picks</span>
+            <span>Library</span>
+            <span>Developers</span>
+            <span>Portal</span>
+          </div>
+          <Button className="bg-black px-8 py-6 text-lg rounded-full">
+            Get started — It&apos;s free
+          </Button>
+        </div>
+
+        {/* Hero */}
+        <div className="flex flex-col space-y-12 justify-center items-center">
+          <span className="flex flex-col text-6xl font-bold gap-1 text-center">
+            Say &lsquo;goodbye&rsquo; to the prompt{" "}
+            <span>fiddling for good</span>
+          </span>
+          <div className="flex space-x-4 items-center">
+            <Button className="px-8 py-6 rounded-full bg-black text-white">
+              See what&apos;s trending
+            </Button>
+            <span>or</span>
+            <Button className="px-8 py-6 rounded-full bg-white/50 backdrop-blur-xl border border-white text-black">
+              Browse our library
+            </Button>
+          </div>
+        </div>
+
+        {/* Bottom */}
+        <div className="flex flex-col space-y-8 items-center justify-center pb-12">
+          <span className="text-lg font-medium">No credit card required</span>
+
+          <div className="flex flex-col space-y-4">
+            {/* round white/15 2comp white logo 2t w/ arrow jb */}
+            <LandingCard
+              title="Write blogs 10x faster"
+              subtitle="FOR BLOG WRITERS"
+              icon={BLOG_WRITER_ICON}
+            />
+            <LandingCard
+              title="Fix all the issues in my code"
+              // this is a callback to my bad ass code
+              subtitle="FOR DEVELOPERS"
+              icon={DEVELOPER_ICON}
+            />
+            <LandingCard
+              title="Select a MMORPG to play"
+              subtitle="FOR GAMERS"
+              icon={GAME_SUGGESTION_ICON}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 left-0 right-0 w-full h-1/2 px-8 z-10">
+        <Image
+          src={"/background.webp"}
+          width={1840}
+          height={1070}
+          alt="Landing graphic image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 }
